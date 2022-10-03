3f190eaf7e92d7e87e186071131c446d4d8a7b77
SanoferSameera <sanofersameera25@gmail.com>
2022-10-03 18:08:09 +0530

    BAH-2401 | Refactor. fix null issue and valid check
    Co-authored-by: Kavitha S <kavitha.s@thoughtworks.com>

--- src/In.ProjectEKA.HipService/Patient/PatientProfileService.cs
+++ src/In.ProjectEKA.HipService/Patient/PatientProfileService.cs
@@ -94,7 +94,7 @@ namespace In.ProjectEKA.HipService.Patient
         {
             var profile = shareProfileRequest?.Profile;
             var demographics = profile?.PatientDemographics;
-            return demographics is {HealthIdNumber: { }, HealthId: { }, Identifiers: { }, Name: { },Gender:{}} && Enum.IsDefined(typeof(Gender), demographics.Gender) && demographics.YearOfBirth != 0;
+            return demographics is {HealthId: { }, Identifiers: { }, Name: { },Gender:{}} && Enum.IsDefined(typeof(Gender), demographics.Gender) && demographics.YearOfBirth != 0;
         }
 
         private async Task<bool> IsExist(string patientIdentifier)
