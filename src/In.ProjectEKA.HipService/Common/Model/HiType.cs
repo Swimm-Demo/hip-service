6762f5c755afd9f55638c7ca8bbfb8c520e2a6b5
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2023-03-06 17:35:17 +0530

    Refactor. removed invalid hitypes (#139)

--- src/In.ProjectEKA.HipService/Common/Model/HiType.cs
+++ src/In.ProjectEKA.HipService/Common/Model/HiType.cs
@@ -2,11 +2,7 @@ namespace In.ProjectEKA.HipService.Common.Model
 {
     public enum HiType
     {
-        Condition,
-        Observation,
         DiagnosticReport,
-        MedicationRequest,
-        DocumentReference,
         Prescription,
         ImmunizationRecord,
         DischargeSummary,
