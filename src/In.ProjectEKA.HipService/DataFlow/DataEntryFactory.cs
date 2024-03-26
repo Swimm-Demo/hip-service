e5757fb5a9f55edf9010b9808a12d3898f097288
SanoferSameera <79590907+SanoferSameera@users.noreply.github.com>
2024-03-26 12:42:43 +0530

    Refactor. updated time zone format (#165)

--- src/In.ProjectEKA.HipService/DataFlow/DataEntryFactory.cs
+++ src/In.ProjectEKA.HipService/DataFlow/DataEntryFactory.cs
@@ -67,7 +67,7 @@ namespace In.ProjectEKA.HipService.DataFlow
                 });
             }
 
-            var keyStructure = new KeyStructure(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"),
+            var keyStructure = new KeyStructure(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.SSSZ"),
                 dataRequestKeyMaterial.DhPublicKey.Parameters, EncryptorHelper.GetPublicKey(keyPair));
             var keyMaterial = new KeyMaterial(dataRequestKeyMaterial.CryptoAlg,
                 dataRequestKeyMaterial.Curve,
